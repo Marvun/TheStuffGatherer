329b9a1e09ee9d127597d37082ceb4abb6c92214
Marv <knoma12@web.de>
2023-09-09 13:15:05 +0200

    updated dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "1.8.10"
+    kotlin("jvm") version "1.9.0"
     application
 }
 
@@ -21,7 +21,7 @@ dependencies {
     testImplementation(kotlin("test"))
 
     implementation("me.jakejmattson:DiscordKt:0.23.4")
-    implementation("dev.kord:kord-core:0.8.0")
+    implementation("dev.kord:kord-core:0.10.0")
 
     implementation("org.jetbrains.exposed:exposed-core:0.40.1")
     implementation("org.jetbrains.exposed:exposed-dao:0.40.1")
